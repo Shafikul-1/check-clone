51880513cbb37c419f625e71a2f12c3226474bc2
MD Shafikul Islam <134792008+Shafikul-1@users.noreply.github.com>
2024-04-02 05:43:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import puppeteer from 'puppeteer';
 import fs from 'fs';
-const dataAll = [];
 
 async function fbDetails(usernames) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:false});
+
     try {
         for (const username of usernames) {
             const page = await browser.newPage();
             let url = '';
+
             if (typeof username === "number") {
                 url = `https://www.facebook.com/profile.php?id=${username}`;
             } else if (typeof username === "string") {
@@ -23,7 +24,7 @@ async function fbDetails(usernames) {
             });
 
             const result = await page.evaluate(() => {
-                const mainPath = "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/div[2]/div/ul/";
+                const mainPath = "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4] /div[2]/div/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/div[2]/div/ul/";
                 const elements = document.evaluate(`${mainPath}div`, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
                 const data = {};
                 for (let i = 0; i < elements.snapshotLength; i++) {
@@ -33,7 +34,24 @@ async function fbDetails(usernames) {
                 data.url = window.location.href;
                 return data;
             });
-            dataAll.push(result);
+
+            // Write data to file
+            const fileName = 'output.json';
+            let existingData = [];
+            try {
+                if (fs.existsSync(fileName)) {
+                    existingData = JSON.parse(fs.readFileSync(fileName));
+                }
+            } catch (err) {
+                console.error("Error reading existing data:", err);
+            }
+
+            // Merge existing data with new data
+            const mergedData = existingData.concat([result]);
+
+            // Write merged data back to file
+            fs.writeFileSync(fileName, JSON.stringify(mergedData, null, 2));
+            console.log(`Data has been Added ${url}`);
 
             await page.close();
         }
@@ -42,23 +60,6 @@ async function fbDetails(usernames) {
     } finally {
         await browser.close();
     }
-    // Read existing data from file
-    let existingData = [];
-    const fileName = 'output.json';
-    try {
-        if (fs.existsSync(fileName)) {
-            existingData = JSON.parse(fs.readFileSync(fileName));
-        }
-    } catch (err) {
-        console.error("Error reading existing data:", err);
-    }
-
-    // Merge existing data with new data
-    const mergedData = existingData.concat(dataAll);
-
-    // Write merged data back to file
-    fs.writeFileSync(fileName, JSON.stringify(mergedData, null, 2));
-    console.log(`Data has been merged and written to ${fileName}`);
 }
 
-fbDetails(['weddingsbymichellelouise', 'knobcreekmeadowsvenue']); // Pass an array of usernames
+fbDetails([100084845875280, 61553161029750, 61557759571804, 100089832105364, 100084168641312]); // Pass an array of usernames
